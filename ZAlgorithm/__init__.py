eef0d66432112b8e1a4b36199d7d3ae2eab76a5b
Justin Barry <justin.barry.e@gmail.com>
2017-08-31 00:43:55 -0400

    Initial commit

---
+++ ZAlgorithm/__init__.py
@@ -0,0 +1,21 @@
+def getzvalues(pattern):
+    zvalues = [];
+    r = 0;
+    for pos in range(1, len(pattern[1:])):
+        for char in pattern:
+            zvalue = 0;
+            if pattern[pos] == char:
+                zvalue += 1;
+                pos+=1;
+            else:
+                if (zvalue )
+                zvalues.append(zvalue);
+                break;
+    return 0;
+
+
+if __name__ == '__main__':
+    for i in range(1, len('justin'[2:])):
+        print(i);
+
+    print('hello2');
