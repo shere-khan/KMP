ceb10268faed282476de3904ddd8dec80d467d3b
Justin-PC\Justin <justin.barry.e>
2017-09-01 10:43:04 -0400

    Fleshing out algorithm. cases laid out. Need to have match_pattern function return mutlitple values

--- ZAlgorithm/__init__.py
+++ ZAlgorithm/__init__.py
@@ -1,34 +1,53 @@
 def getzvalues(pattern):
-    zvalues = [];
-    r = 0;
-    for pos in range(1, len(pattern[1:])):
-        if (pos > r):
-            #manually compute
-            pass
-        if (pos <= r):
-            zkprime = "";
-            beta = "";
-            if (zkprime < beta):
-                pass
-            if (zkprime > beta):
-                pass
-            if (zkprime == beta):
+    zvalues = []
+    r = 0
+    l = 0
+    for k in range(1, len(pattern[1:])):
+        # Case 1: manually compute
+        if k > r:
+            match_pattern(pattern, zvalues, 0, k, r, l)
+
+        # Case 2
+        elif k <= r:
+            zkprime = k - l + 1
+            beta = r - k
+
+            # Case 2a: zkprime = zk
+            if zkprime < beta:
                 pass
 
-        for char in pattern:
-            zvalue = 0;
-            if pattern[pos] == char:
-                zvalue += 1;
-                pos+=1;
+            # Case 2b: zkprime = beta
+            if zkprime > beta:
+                pass
+
+            # Case 2c: zkprime = beta + matched strings starting
+            # at position zkprime + kprime + 1
+            kprime = "" # TODO ??
+            pos1 = kprime + zkprime + -1
+            pos2 = k + beta + 1
+            if zkprime == beta:
+                match_pattern(pattern, zvalues, pos1, pos2, r)
+
+
+def match_pattern(pattern, zvalues, pos1, pos2, r, l):
+    for k in range(pos2, len(pattern[pos2:])):
+        for char in range(pos1, len(pattern[pos1:])):
+            zvalue = 0
+            # matches
+            if pattern[k] == char:
+                zvalue += 1
+                k+=1
+            # mismatch reached
             else:
-                if (zvalue )
-                zvalues.append(zvalue);
-                break;
-    return 0;
-
+                new_r = k + zvalue - 1
+                if new_r > r:
+                    r = new_r
+                    l = k
+                zvalues.append(zvalue)
+                break
 
 if __name__ == '__main__':
     for i in range(1, len('justin'[2:])):
-        print(i);
+        print(i)
 
-    print('hello2');
+    print('hello2')
