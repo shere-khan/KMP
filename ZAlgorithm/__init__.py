0571974d4979ba684fd5af89954f952267b9d213
Justin-PC\Justin <justin.barry.e>
2017-09-01 23:44:38 -0400

    Fleshed out most of the program and did preliminary testing. TODO needs to be completed for ending substring but existing suffixes still need to be checked for matches

--- ZAlgorithm/__init__.py
+++ ZAlgorithm/__init__.py
@@ -6,7 +6,8 @@ def getzvalues(pattern):
     for k in range(1, len(pattern[1:])):
         # Case 1: manually compute
         if k > r:
-            l, r = match_pattern(pattern, zvalues, 0, k, r)
+            l, r, zvalue = match_pattern(pattern, 0, k, r, l)
+            zvalues.append(zvalue)
 
         # Case 2
         elif k <= r:
@@ -27,34 +28,46 @@ def getzvalues(pattern):
             # Case 2c: if zkprime is equal to beta, zkprime equals the length
             # of beta plus the number of matched strings starting
             # at position zkprime + kprime + 1 until a mismatch is reached
-            pos1 = kprime + beta + 1
-            pos2 = k + beta + 1
             if zkprime == beta:
-                l, k = match_pattern(pattern, zvalues, pos1, pos2, r)
+                pos1 = kprime + beta
+                pos2 = k + beta
+                l, k, zvalue = match_pattern(pattern, pos1, pos2, r, l)
+                zvalues.append(zvalue + beta)
     return zvalues
 
 
 # pos1 - index on the kprime side
 # pos2 - index on the k side
-def match_pattern(pattern, zvalues, pos1, pos2, r):
+def match_pattern(pattern, pos1, pos2, r, l):
+    # print(len(pattern))
+    # if pos2 > len(pattern) - 1:
+    #     return l, r
     zvalue = 0
+    match = False
     for i in pattern[pos2:]:
         # for i in range(pos2, len(pattern[pos2:])):
         # matches
         if i == pattern[pos1]:
+            match = True
             zvalue += 1
             pos1 += 1
         # mismatch reached
         else:
             new_r = pos2 + zvalue - 1
-            if new_r > r:
+            if match and new_r > r:
                 r = new_r
-            zvalues.append(zvalue)
-            return pos2, r
+                return pos2, r, zvalue
+            return l, r, zvalue
+    # TODO: flesh out his case when the end of the string
+    # is reached with matching characters but there are
+    # still suffixes of the string that need zk values
+    return l, r, 0
+
 
 if __name__ == '__main__':
     text = 'abababxysldkjfabababaslkdjfabababa'
     pattern = 'abababa'
     text_pattern = pattern + '$' + text
+    print(len(text_pattern))
     zvalues = getzvalues(text_pattern)
     print(zvalues)
